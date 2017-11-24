e45a51aec45262df0e2b7e2dba0d0d97c4b34ed2
JRixGeromo <jrixgeromo@gmail.com>
2017-11-24 21:47:42 +0800

    updates

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { DropdownSettings } from '../ng-multiselect-dropdown/src/multiselect.component';
 
 @Component({
     selector: 'my-app',
--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { UiSwitchModule } from 'angular2-ui-switch';
-import { Ng2MultiSelectDropDownModule } from 'ng-multiselect-dropdown';
-import { SelectSectionComponent } from './multiclient/select-section';
-import { ClientSectionComponent } from './multiclient/client-section.component';
-import { MultipleClientComponent } from './multiclient/select/multiple-client';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from "./app.component";
@@ -12,12 +9,10 @@ import { AppComponent } from "./app.component";
 @NgModule({
     declarations: [
         AppComponent,
-        ClientSectionComponent,
-        MultipleClientComponent,
-        SelectSectionComponent
+
     ],
     imports: [FormsModule,
-        ReactiveFormsModule, BrowserModule, UiSwitchModule, Ng2MultiSelectDropDownModule.forRoot()],
+        ReactiveFormsModule, BrowserModule, UiSwitchModule,],
     bootstrap: [AppComponent]
 })
 export class AppModule {
